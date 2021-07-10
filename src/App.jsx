c557343e33dcddda204fd7599c5c52557732f9cf
keigo9 <61878416+keigo9@users.noreply.github.com>
2021-07-10 23:33:10 +0900

    Feature/over5task

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,13 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>
+          登録できるtodoは５個までです。消化しましょう。
+        </p>
+      )}
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
