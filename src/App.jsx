edcc1b30229c2789728abf7a888a91cb28f79899
keigo9 <61878416+keigo9@users.noreply.github.com>
2021-07-10 22:05:34 +0900

    Feature/done

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,16 @@ export const App = () => {
     setIncompleteTodos(newTodos);
   };
 
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
+
   return (
     <>
       <div className="input-area">
@@ -38,7 +48,7 @@ export const App = () => {
             return (
               <li key={todo} className="list-row">
                 <div>{todo}</div>
-                <button>完了</button>
+                <button onClick={() => onClickComplete(index)}>完了</button>
                 <button onClick={() => onClickDelete(index)}>削除</button>
               </li>
             );
